1bca0dee67411220d7d551d66d12f98af91e4057
a.smirnov <a.smirnov@cometrica.ru>
2021-08-25 21:42:47 +0300

    lesson3

--- src/main/java/ru/geekbrains/summer/market/SummerMarketApplication.java
+++ src/main/java/ru/geekbrains/summer/market/SummerMarketApplication.java
@@ -5,21 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SummerMarketApplication {
-	// Домашнее задание:
-	// 1. Добавить фильтр по максимальной цене
-	// 2. Убрать построение спецификаций куда-нибудь из контроллера
-	// 3. * Добавить форму фильтра на фронт
-
-	// План по магазину:
-	// 1. Привязка корзины к пользователю, подключение Reddis
-	// 2. Рассылка писем
-	// 3. Админка
-	// 4. Swagger
-	// + 5. Фильтры по товарам
-	// 6. Маппинг DTO <-> Entity, @EntityGraph
-	// 7. Картинки
-	// 8. Промо-коды
-	// 9.
+	//1. AOP: С помощью АОП посчитайте по каждому сервису суммарное время, уходящее на выполнение методов этих сервисов. И по endpoint'у /statistic выдайте полученную статистику клиенту. Пример:
+	//	ProductService: 1200 ms
+	//	OrderService: 95 ms
+	//	UserService: 2000 ms
 
 	public static void main(String[] args) {
 		SpringApplication.run(SummerMarketApplication.class, args);
